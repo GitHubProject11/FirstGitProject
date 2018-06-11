f16811edc5a1c654d63faa45332f1ba64d272fec
nani <nani@gmail.com>
2018-06-11 18:08:00 +0530

    commit by TL

---
+++ FirstApp/src/com/nt/test/Sample.java
@@ -0,0 +1,13 @@
+package com.nt.test;
+
+public class Sample {
+    public String wishMsg(String name){
+    	return "good evening "+name;
+    }
+	public static void main(String[] args) {
+		Sample s=new Sample();
+		System.out.println(s.wishMsg("satya"));
+
+	}
+
+}
